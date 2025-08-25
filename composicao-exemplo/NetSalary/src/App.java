b8dea868aee2154e54ecc6524bffab1ca8d274c4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-25 15:42:00 -0300

    System example without components

--- composicao-exemplo/NetSalary/src/App.java
+++ composicao-exemplo/NetSalary/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        double grossSalary, netSalary;
+
+        System.out.print("Nome: ");
+        name = sc.nextLine();
+        System.out.print("Salario bruto: ");
+        grossSalary = sc.nextDouble();
+
+        netSalary = grossSalary - (grossSalary * 0.30);
+
+        System.out.println();
+        System.out.println("Salario liquido: " + String.format("%.2f", netSalary));
+        sc.close();
     }
 }
