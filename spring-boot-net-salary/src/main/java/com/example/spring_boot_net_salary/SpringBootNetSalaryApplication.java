3f6c34c0a04f4e49a358b63b7dfdf394668ca001
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-25 17:26:09 -0300

    Project creation with Spring Boot

---
+++ spring-boot-net-salary/src/main/java/com/example/spring_boot_net_salary/SpringBootNetSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_net_salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNetSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNetSalaryApplication.class, args);
+	}
+
+}
---
+++ spring-boot-net-salary/src/test/java/com/example/spring_boot_net_salary/SpringBootNetSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_net_salary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNetSalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
