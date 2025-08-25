09bba3d015a5000b5929b0ab57733465ea8fb324
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-25 19:27:11 -0300

    Project implementation without components

--- composicao-exemplo/OrderComposition/src/App.java
+++ composicao-exemplo/OrderComposition/src/App.java
@@ -1,5 +1,33 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int code;
+        double value, discountPercent, valueWithDiscount, total;
+
+        code = sc.nextInt();
+        value = sc.nextDouble();
+        discountPercent = sc.nextDouble();
+
+        valueWithDiscount = value - (value * discountPercent/100);
+
+        if (valueWithDiscount < 100) {
+            total = valueWithDiscount + 20.00;
+        } else if(valueWithDiscount >= 100 && valueWithDiscount < 200) {
+            total = valueWithDiscount + 12.00;
+        } else {
+            total = valueWithDiscount;
+        }
+
+        System.out.println();
+        System.out.println("Pedido codigo " + code);
+        System.out.println("Valor total: R$ " + String.format("%.2f", total));
+
+        sc.close();
     }
 }
